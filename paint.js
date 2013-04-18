2039b308125e23fbf63fc41a058aa4e511699831
Jim Hart <jimhart3000@gmail.com>
2013-04-18 06:10:10 -0500

    can draw when off canvas, but x/y position when scrolled is screwy

--- paint.js
+++ paint.js
@@ -1,5 +1,6 @@
 (function(){
     var canvas = document.getElementById('canvas'),
+        body = document.getElementsByTagName('body'),
         canvasContext = canvas.getContext('2d'),
         strokeColor = '#000000',
         strokeWeight = 1,
@@ -8,6 +9,14 @@
             canvasContext.lineTo(evt.clientX - 8, evt.clientY - 8);
             canvasContext.stroke();
         };
+    var mouseUpHandler = function(evt) {
+        console.log(evt);
+        evt.preventDefault();
+        canvasContext.lineTo(evt.clientX - 8, evt.clientY - 8);
+        canvasContext.stroke();
+        window.removeEventListener('mousemove', mouseMoveHandler);
+    }
+    console.log(body[0]);
     var colorButtons = document.getElementsByClassName('colorButton');
     var clearButton = document.getElementById('clear');
     for (var i = 0; i < colorButtons.length; i++) {
@@ -26,13 +35,10 @@
         canvasContext.beginPath();
         console.log(evt);
         canvasContext.moveTo(evt.clientX - 8, evt.clientY - 8);
-        canvas.addEventListener('mousemove', mouseMoveHandler);
+        window.addEventListener('mousemove', mouseMoveHandler);
+        canvas.addEventListener('mouseup', mouseUpHandler);
+        
     });
-    canvas.addEventListener('mouseup', function(evt){
-        evt.preventDefault();
-        canvasContext.lineTo(evt.clientX - 8, evt.clientY - 8);
-        canvasContext.stroke();
-        canvas.removeEventListener('mousemove', mouseMoveHandler);
-    });
+    
     console.log(canvasContext);
 })();
